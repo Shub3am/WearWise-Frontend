816b32012e478121b4abe86cf848dfd9931579d4
shub3am <shubhamvishwakarma0604@gmail.com>
2025-01-24 11:41:59 +0530

    Added Smart Watch Sample Data

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,364 @@ export default function Home() {
 
   const [uploadedWatchMetric, setUploadedWatchMetric] = useState({});
   const [HealthData, setHealthData] = useState([]);
+  const [ShowSampleData, SetShowSampleData] = useState(true);
 
+  let sampleData = {
+    data: {
+      metrics: [
+        {
+          name: "apple_exercise_time",
+          data: [
+            {
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 18,
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 26,
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+            },
+          ],
+          units: "min",
+        },
+        {
+          units: "km/hr",
+          data: [
+            {
+              qty: 4.247999999999999,
+              date: "2025-01-20 00:00:00 +0530",
+            },
+            {
+              qty: 4.23,
+              date: "2025-01-21 00:00:00 +0530",
+            },
+          ],
+          name: "walking_speed",
+        },
+        {
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 13,
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 11,
+            },
+          ],
+          name: "apple_stand_hour",
+          units: "count",
+        },
+        {
+          name: "basal_energy_burned",
+          units: "kJ",
+          data: [
+            {
+              qty: 9235.335790047113,
+              date: "2025-01-20 00:00:00 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+            },
+            {
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 9283.215714489832,
+            },
+          ],
+        },
+        {
+          name: "walking_heart_rate_average",
+          units: "count/min",
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 137,
+            },
+            {
+              qty: 117,
+              date: "2025-01-21 00:00:00 +0530",
+            },
+          ],
+        },
+        {
+          units: "count/min",
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              Max: 160,
+              Avg: 80.4174567780962,
+              Min: 51,
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+            },
+            {
+              Max: 169.00000000000003,
+              Min: 51,
+              date: "2025-01-21 00:00:00 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              Avg: 78.75381714198274,
+            },
+          ],
+          name: "heart_rate",
+        },
+        {
+          units: "km",
+          name: "walking_running_distance",
+          data: [
+            {
+              source:
+                "Shubham\u2019s Apple\u00a0Watch 9|Shubham\u2019s iPhone 12",
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 4.496227059594937,
+            },
+            {
+              qty: 3.576600278553045,
+              date: "2025-01-21 00:00:00 +0530",
+              source:
+                "Shubham\u2019s Apple\u00a0Watch 9|Shubham\u2019s iPhone 12",
+            },
+          ],
+        },
+        {
+          units: "kJ",
+          name: "active_energy",
+          data: [
+            {
+              qty: 2330.3290080000015,
+              date: "2025-01-20 00:00:00 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+            },
+            {
+              qty: 2426.464802527237,
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              date: "2025-01-21 00:00:00 +0530",
+            },
+          ],
+        },
+        {
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              source:
+                "Shubham\u2019s Apple\u00a0Watch 9|Shubham\u2019s iPhone 12",
+              qty: 26,
+            },
+            {
+              source:
+                "Shubham\u2019s Apple\u00a0Watch 9|Shubham\u2019s iPhone 12",
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 27.126519895330638,
+            },
+          ],
+          units: "count",
+          name: "flights_climbed",
+        },
+        {
+          name: "apple_stand_time",
+          units: "min",
+          data: [
+            {
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              qty: 139,
+              date: "2025-01-20 00:00:00 +0530",
+            },
+            {
+              qty: 93,
+              date: "2025-01-21 00:00:00 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+            },
+          ],
+        },
+        {
+          name: "step_count",
+          units: "count",
+          data: [
+            {
+              source:
+                "Shubham\u2019s Apple\u00a0Watch 9|Shubham\u2019s iPhone 12",
+              qty: 5793,
+              date: "2025-01-20 00:00:00 +0530",
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 4489.137096909537,
+              source:
+                "Shubham\u2019s Apple\u00a0Watch 9|Shubham\u2019s iPhone 12",
+            },
+          ],
+        },
+        {
+          units: "%",
+          name: "blood_oxygen_saturation",
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 97.8695652173913,
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 96.84210526315789,
+            },
+          ],
+        },
+        {
+          name: "sleep_analysis",
+          data: [
+            {
+              sleepEnd: "2025-01-20 17:44:40 +0530",
+              inBedStart: "2025-01-20 01:56:58 +0530",
+              awake: 0.4833333333333334,
+              inBedEnd: "2025-01-20 17:44:40 +0530",
+              sleepStart: "2025-01-20 01:56:58 +0530",
+              rem: 0.6083333333333334,
+              asleep: 2.975,
+              inBed: 0,
+              date: "2025-01-20 17:19:40 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              core: 3.7583333333333333,
+              deep: 0.8833333333333333,
+            },
+            {
+              sleepEnd: "2025-01-21 09:00:30 +0530",
+              inBedStart: "2025-01-20 23:54:16 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              rem: 0.8166666666666665,
+              asleep: 1.1833333333333333,
+              inBedEnd: "2025-01-21 09:00:30 +0530",
+              sleepStart: "2025-01-20 23:54:16 +0530",
+              core: 5.6833333333333345,
+              inBed: 0,
+              date: "2025-01-21 08:54:00 +0530",
+              deep: 0.8833333333333333,
+              awake: 0.3,
+            },
+          ],
+          units: "hr",
+        },
+        {
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 36.41770362854004,
+            },
+            {
+              qty: 36.204750061035156,
+              date: "2025-01-21 00:00:00 +0530",
+            },
+          ],
+          name: "apple_sleeping_wrist_temperature",
+          units: "degC",
+        },
+        {
+          units: "count/min",
+          data: [
+            {
+              qty: 15.56578947368421,
+              date: "2025-01-20 00:00:00 +0530",
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 15.7,
+            },
+          ],
+          name: "respiratory_rate",
+        },
+        {
+          name: "time_in_daylight",
+          units: "min",
+          data: [
+            {
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+              qty: 232,
+              date: "2025-01-20 00:00:00 +0530",
+            },
+            {
+              qty: 145,
+              date: "2025-01-21 00:00:00 +0530",
+              source: "Shubham\u2019s Apple\u00a0Watch 9",
+            },
+          ],
+        },
+        {
+          units: "count/min",
+          data: [
+            {
+              qty: 63,
+              date: "2025-01-20 00:00:00 +0530",
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 67,
+            },
+          ],
+          name: "resting_heart_rate",
+        },
+        {
+          name: "heart_rate_variability",
+          units: "ms",
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 47.70441711496111,
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 76.1597494440531,
+            },
+          ],
+        },
+        {
+          units: "dBASPL",
+          name: "environmental_audio_exposure",
+          data: [
+            {
+              date: "2025-01-19 00:00:00 +0530",
+              qty: 56.25604509654114,
+            },
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 66.25216526981563,
+            },
+            {
+              qty: 64.21051742692954,
+              date: "2025-01-21 00:00:00 +0530",
+            },
+          ],
+        },
+        {
+          name: "headphone_audio_exposure",
+          data: [
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 70.31747862321325,
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 69.98808300599272,
+            },
+          ],
+          units: "dBASPL",
+        },
+        {
+          units: "kcal/hr\u00b7kg",
+          name: "physical_effort",
+          data: [
+            {
+              qty: 1.600000023841858,
+              date: "2025-01-19 00:00:00 +0530",
+            },
+            {
+              date: "2025-01-20 00:00:00 +0530",
+              qty: 3.434335846948743,
+            },
+            {
+              date: "2025-01-21 00:00:00 +0530",
+              qty: 3.734300811875779,
+            },
+          ],
+        },
+      ],
+    },
+  };
   const handleChange = (e) => {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
@@ -155,7 +512,7 @@ export default function Home() {
         {/* About Section */}
         <section className="mb-12 text-center">
           <h2 className="text-3xl font-bold mb-4 bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
-            About WearWises
+            About WearWise
           </h2>
           <p className="text-gray-600 text-lg max-w-3xl mx-auto">
             WearWise is an innovative AI Health Diagnosing Application that
@@ -406,6 +763,32 @@ export default function Home() {
                   type="submit"
                   className="w-full bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700">
                   Submit Questionnaire
+                </Button>
+                <div>Sample Watch Data</div>
+                <div className="relative">
+                  {ShowSampleData ? (
+                    <div className="flex  bg-white w-4/6 h-4/6 items-center shadow-lg rounded-md fixed p-5 m-auto flex-col top-10  whitespace-pre overflow-y-scroll">
+                      <button
+                        onClick={() => {
+                          SetShowSampleData(!ShowSampleData);
+                        }}
+                        className="text-white p-2 border absolute mt-1 w-5/6 bg-red-600 text-center m-auto flex justify-center items-center">
+                        Close Popup
+                      </button>
+                      <div className=" mt-14">
+                        {" "}
+                        {JSON.stringify(sampleData, null, "\t")}
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+                <Button
+                  type="button"
+                  onClick={() => {
+                    SetShowSampleData(!ShowSampleData);
+                  }}
+                  className="w-full bg-gradient-to-r from-green-600 to-green-600 ">
+                  Check Sample Smart Watch's Data
                 </Button>
               </form>
             </Form>
